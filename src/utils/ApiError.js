292f34257d88a42a107c15145578784dd50e6fc1
Abhishek <Abhishekraut8697@gmail.com>
2025-02-02 12:32:46 +0530

    fit:added ApiError ApiResponse AsyncHandler utility

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class APIError extends Error {
+    constructor(
+      statusCode,
+      message = "Something went wrong",
+      type = "UNKNOWN",
+      errors = [],
+      stack = ""
+    ) {
+      super(message);
+      this.statusCode = statusCode;
+      this.data = null;
+      this.type = type;
+      this.success = false;
+      this.errors = errors;
+  
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+      }
+    }
+  }
+  
+  export { APIError };
+  
